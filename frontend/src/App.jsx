b27defaaf84fc23a5dea2a71663848b7d7de8623
VishakhaKayal <mgpsvishakha7944@gmail.com>
2024-08-08 20:39:45 +0530

    updated App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,9 +14,8 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import NavbarRegistered from "./components/Home/NavbarRegistered";
-// import useWindowWidth from "./components/hooks/useWindowWidth";
-// export const url = "https://event360.onrender.com";
-export const url = "http://localhost:3000";
+export const url = "https://event360.onrender.com";
+
 
 
 function App() {
@@ -56,7 +55,7 @@ function App() {
 
   return (
     <>
-      {/* <SplashScreen/> */}
+      <SplashScreen/>
       <main className="w-full h-full  bg-[#7e79672c]">
         <Routes>
           <Route path="/Signup" element={<CreateAccount onSignup={handleSignup} />} />
