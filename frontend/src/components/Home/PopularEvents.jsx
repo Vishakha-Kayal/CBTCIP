4aa1c88bddb279144de50eb806a7b3db812d05ac
VishakhaKayal <mgpsvishakha7944@gmail.com>
2024-08-24 14:28:47 +0530

    removedd loading

--- frontend/src/components/Home/PopularEvents.jsx
+++ frontend/src/components/Home/PopularEvents.jsx
@@ -1,45 +1,17 @@
 import { assets, formatTime } from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 
 const PopularEvents = ({ eventDetails }) => {
   const navigate = useNavigate();
   const [eventType, setEventType] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    let loadingToast;
-    if (isLoading) {
-      loadingToast = toast.loading("Fetching events...");
-    }
-    
-    // Check if eventDetails is not empty
-    if (eventDetails && eventDetails.length > 0) {
+    if (eventDetails.length > 0) {
       setEventType(eventDetails[0].eventType);
-      setIsLoading(false);
-      if (isLoading==false) {
-        toast.dismiss(loadingToast);
-        toast.success("Events loaded successfully!");
-      }
-    } else {
-      // If eventDetails is empty after a certain time, show an error
-      const timeoutId = setTimeout(() => {
-        if (isLoading) {
-          setIsLoading(false);
-          if (isLoading==false) {
-            toast.dismiss(loadingToast);
-          }
-          toast.error("No events found. Please try again later.");
-        }
-      }, 10000); // 10 seconds timeout
-
-      // Cleanup function
-      return () => clearTimeout(timeoutId);
     }
-  }, [eventDetails]); 
+  }, [eventDetails]);
 
   // Check if eventDetails is empty
   if (eventDetails.length === 0) {
@@ -48,73 +20,69 @@ const PopularEvents = ({ eventDetails }) => {
 
   return (
     <>
-    <ToastContainer position="top-right" autoClose={3000} />
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        eventDetails.slice(0, 6).map((event, i) => {
-          const dateObj = new Date(event.startDate);
-          const monthNames = [
-            "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-            "JUL", "AUG", "SEP", "OCT", "NOV", "DEC",
-          ];
-          const monthIndex = dateObj.getUTCMonth();
-          const monthAbbreviation = monthNames[monthIndex];
+      {eventDetails.slice(0, 6).map((event, i) => {
+        const dateObj = new Date(event.startDate);
+        const monthNames = [
+          "JAN", "FEB", "MAR", "APR", "MAY", "JUN", 
+          "JUL", "AUG", "SEP", "OCT", "NOV", "DEC",
+        ];
+        const monthIndex = dateObj.getUTCMonth();
+        const monthAbbreviation = monthNames[monthIndex];
 
-          const date = dateObj.getUTCDate();
-          const endDate = new Date(event.endDate).getUTCDate();
+        const date = dateObj.getUTCDate();
+        const endDate = new Date(event.endDate).getUTCDate();
 
-          return (
-            <div
-              className="eventDiv flex flex-col w-full mb-2 cursor-pointer hover:bg-[#f7f6f5] transition-all ease-linear 2s h-[27rem]"
-              key={i}
-              onClick={() => navigate(`/api/eventpage/${event.id}`)}
-            >
-              <div className="w-full h-[60%] overflow-hidden relative">
-                <img
-                  src={event.banner}
-                  alt={event.name}
-                  className="w-full h-full"
-                />
-                <div className="bg-[#FFE047] px-2 h-[2.188rem] absolute bottom-0 rounded-tr-md flex justify-center items-center">
-                  <p className="font-secondary text-lg font-light">
-                    {event.category}
-                  </p>
-                </div>
-              </div>
-              <div className="h-[40%] md:px-3 flex gap-6 mt-3 px-3 w-full font-['open sans']">
-                <div className="w-[25%] flex flex-col">
-                  <h2 className="text-[#4539b4] text-center font-primary text-2xl font-semibold leading-[normal]">
-                    {monthAbbreviation}
-                  </h2>
-                  <h2 className="text-[#2d2c3c] text-center font-primary text-[1.625rem] font-bold leading-[normal]">
-                    {date === endDate ? `${date}` : `${date}-${endDate}`}
-                  </h2>
-                </div>
-                <div className="w-[75%] flex flex-col gap-1">
-                  <div className="w-[22rem] overflow-hidden text-[#2d2c3c] text-xl font-semibold leading-[normal] line-clamp-2">
-                    {event.name}
-                  </div>
-                  <div className="text-[#5a5a5a] text-base font-semibold leading-[normal]">
-                    {event.location}
-                  </div>
-                  <div className="w-[11.875rem] text-[#5a5a5a] text-base leading-[normal] uppercase">
-                    <span>{formatTime(event.startTime)} </span>-{" "}
-                    <span>{formatTime(event.endTime)}</span>
-                  </div>
-                  <section className="flex mt-1">
-                    <div className="w-6 h-6 rounded-full">
-                      <img src={assets.ticketevent} alt="" className="w-5 h-5" />
-                    </div>
-                    <div className="overflow-hidden text-[#5a5a5a] text-sm font-semibold leading-[normal]">
-                      {event.eventType !== "ticketed" ? "Free" : `INR ${event.ticketPrice}`}
-                    </div>
-                  </section>
-                </div>
+        return (
+          <div
+            className="eventDiv flex flex-col w-full mb-2 cursor-pointer hover:bg-[#f7f6f5] transition-all ease-linear 2s h-[27rem]"
+            key={i}
+            onClick={() => navigate(`/api/eventpage/${event.id}`)}
+          >
+            <div className="w-full h-[60%] overflow-hidden relative">
+              <img
+                src={event.banner}
+                alt={event.name}
+                className="w-full h-full"
+              />
+              <div className="bg-[#FFE047] px-2 h-[2.188rem] absolute bottom-0 rounded-tr-md flex justify-center items-center">
+                <p className="font-secondary text-lg font-light">
+                  {event.category}
+                </p>
               </div>
             </div>
-          );
-        }))}
+            <div className="h-[40%] md:px-3 flex gap-6 mt-3 px-3 w-full font-['open sans']">
+              <div className="w-[25%] flex flex-col">
+                <h2 className="text-[#4539b4] text-center font-primary text-2xl font-semibold leading-[normal]">
+                  {monthAbbreviation}
+                </h2>
+                <h2 className="text-[#2d2c3c] text-center font-primary text-[1.625rem] font-bold leading-[normal]">
+                  {date === endDate ? `${date}` : `${date}-${endDate}`}
+                </h2>
+              </div>
+              <div className="w-[75%] flex flex-col gap-1">
+                <div className="w-[22rem] overflow-hidden text-[#2d2c3c] text-xl font-semibold leading-[normal] line-clamp-2">
+                  {event.name}
+                </div>
+                <div className="text-[#5a5a5a] text-base font-semibold leading-[normal]">
+                  {event.location}
+                </div>
+                <div className="w-[11.875rem] text-[#5a5a5a] text-base leading-[normal] uppercase">
+                  <span>{formatTime(event.startTime)} </span>-{" "}
+                  <span>{formatTime(event.endTime)}</span>
+                </div>
+                <section className="flex mt-1">
+                  <div className="w-6 h-6 rounded-full">
+                    <img src={assets.ticketevent} alt="" className="w-5 h-5" />
+                  </div>
+                  <div className="overflow-hidden text-[#5a5a5a] text-sm font-semibold leading-[normal]">
+                    {event.eventType !== "ticketed" ? "Free" : `INR ${event.ticketPrice}`}
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 };
