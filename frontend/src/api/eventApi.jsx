470eb651df8c7289e4a94e9d91c5bd6aaf7748c9
VishakhaKayal <mgpsvishakha7944@gmail.com>
2024-09-28 20:34:44 +0530

    updated

--- frontend/src/api/eventApi.jsx
+++ frontend/src/api/eventApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
-const url = "http://localhost:3000";
-// const url = "https://event360.onrender.com";
+// const url = "http://localhost:3000";
+const url = "https://event360.onrender.com";
 
 const api = axios.create({
     baseURL:`${url}`,
